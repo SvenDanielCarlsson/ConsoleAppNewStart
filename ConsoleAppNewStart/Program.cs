e6ed838489b1a8a8769f33427e251f5acbfd4880
Sven.daniel.carlsson@gmail.com <Sven.daniel.carlsson@gmail.com>
2021-08-30 16:48:19 +0200

    Added TryParse for userAgeString

--- ConsoleAppNewStart/Program.cs
+++ ConsoleAppNewStart/Program.cs
@@ -6,8 +6,38 @@ namespace ConsoleAppNewStart
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("I want to learn");
+
+            string userAgeString = AskUserFor("your age");
+            Console.Clear();
+
+            while (!int.TryParse(userAgeString, out int result))
+            {                
+                Console.WriteLine("'"+ userAgeString + "' is an unvalid input");                                
+                userAgeString = AskUserFor("your age");
+                Console.Clear();
+            }
+
+
+            Console.WriteLine("You typed in '" + userAgeString + "' as your age");
+            
+            int userAge = Convert.ToInt32(userAgeString);
+            if (userAge >= 18)
+            {
+                Console.WriteLine("Welcome to the bar, please enjoy");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, try again when you're welcome by default of age");
+            }
+        }
+
+        static string AskUserFor(string what)
+        {
+            Console.Write("Please enter " + what + ": ");
+
+            string userInput = Console.ReadLine();
+
+            return userInput;
         }
     }
 }
