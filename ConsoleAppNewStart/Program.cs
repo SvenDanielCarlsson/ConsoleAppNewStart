0339f6b51aae913c5ef01ef76b85ae0c3ce7de0e
Sven.daniel.carlsson@gmail.com <Sven.daniel.carlsson@gmail.com>
2021-09-02 10:02:36 +0200

    Added menu and methods, uppgraded loop in AskUserForUser method

--- ConsoleAppNewStart/Program.cs
+++ ConsoleAppNewStart/Program.cs
@@ -6,30 +6,51 @@ namespace ConsoleAppNewStart
     {
         static void Main(string[] args)
         {
-
-            string userAgeString = AskUserFor("your age");
-            Console.Clear();
-
-            while (!int.TryParse(userAgeString, out int result))
-            {                
-                Console.WriteLine("'"+ userAgeString + "' is an unvalid input");                                
-                userAgeString = AskUserFor("your age");
+            bool keepLooping = true;
+            while (keepLooping)
+            {
                 Console.Clear();
+                Console.WriteLine("( - - - M E N U - - - )"
+                                    + "\n1. Age Limited Store"
+                                    );
+
+
+                int selection = AskUserForNumber("your selection");
+                switch (selection)
+                {
+                    case 1:
+                        Console.Clear();
+                        AgeLimitStore();
+                        break;
+
+                    default:
+                        Console.WriteLine("\nIncorrect selection");
+                        break;
+                }
+
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey(true);
             }
+        }
 
 
-            Console.WriteLine("You typed in '" + userAgeString + "' as your age");
-            
-            int userAge = Convert.ToInt32(userAgeString);
+        static void AgeLimitStore()
+        {
+            Console.WriteLine("~ Age Limited Store ~");
+
+            int userAge = AskUserForNumber("your age");
+            Console.WriteLine("\nYou proclaimed " + userAge + " as your age");
+
             if (userAge >= 18)
             {
-                Console.WriteLine("Welcome to the bar, please enjoy");
+                Console.WriteLine("Welcome to the bar, please enjoy\n");
             }
             else
             {
-                Console.WriteLine("Sorry, try again when you're welcome by default of age");
+                Console.WriteLine("Sorry, try again when you're welcome by default of age\n");
             }
         }
+
 
         static string AskUserFor(string what)
         {
@@ -38,6 +59,41 @@ namespace ConsoleAppNewStart
             string userInput = Console.ReadLine();
 
             return userInput;
+        }
+
+
+        static int AskUserForNumber(string what)
+        {
+            bool wasNotNumber = true;
+            int result = 0;
+            string testInput;
+
+            do
+            {
+                testInput = AskUserFor(what);
+                wasNotNumber = !int.TryParse(testInput, out result);
+
+                if (wasNotNumber == true)
+                {
+                    Console.WriteLine("\nType using numbers");
+                    Console.WriteLine("'" + testInput + "' is an invalid input\n");
+                }
+
+            } while (wasNotNumber);
+
+
+            return result;
+
+            /*
+            string userAgeString = AskUserFor("your age");
+            
+            while (!int.TryParse(userAgeString, out int result))
+            {
+                Console.WriteLine("'" + userAgeString + "' is an invalid input");
+                userAgeString = AskUserFor("your age");
+                Console.Clear();
+            }
+            */
         }
     }
 }
